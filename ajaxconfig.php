98a4753c779f118218f90fb35557d0173ffcd5aa
Rabi-Jerome <rabijerome.p@gmail.com>
2025-05-21 12:13:19 +0530

    correction on download access
    In API return json value set as intval because in js condition checking datatype value.

--- ajaxconfig.php
+++ ajaxconfig.php
@@ -1,8 +1,8 @@
 <?php
 $timeZoneQry = "SET time_zone = '+5:30' ";
 $host = "localhost";
-$db_user = "db_user";
-$db_pass = "dbpassword@123";
+$db_user = "root";
+$db_pass = "";
 $dbname = "micro_finance";
 $pdo = new PDO("mysql:host=$host; dbname=$dbname", $db_user, $db_pass);
 $pdo->exec($timeZoneQry);
